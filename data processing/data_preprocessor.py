953de849faf9ec05426e0f2ca291026571636844
chewterence <chewterence@hotmail.com>
2022-11-02 18:27:01 +0800

    update pre-processing steps

--- data processing/data_preprocessor.py
+++ data processing/data_preprocessor.py
@@ -2,6 +2,13 @@ import pandas as pd
 import math
 import json
 # ==========================================================================
+# This file does the pre-processing step for the data:
+# It takes in the raw UNHCR format of resettlement_data.csv
+# It also takes in country_coordinates.csv which contains a list of countries mapping to their geo coordinate values
+# The output of this pre-processing step is line_coordinates.json which contains the correctly processed data
+# invalid_coordinates_dataframe.csv is a bi-product of the pre-processing step that would be processed separately
+
+# ==========================================================================
 ##### RAW DATA FORM
 
 df = pd.read_csv("resettlement_data.csv")
@@ -17,6 +24,11 @@ coords_dict = dict(zip(country_coords.name, zip(country_coords.longitude, countr
 # Convert country of origin and asylum to geo coordinates
 df['coor_from'] = df['Origin'].map(coords_dict)
 df['coor_to'] = df['Country / territory of asylum/residence'].map(coords_dict)
+
+# Export the data frames with invalid coordinates for manual processing
+# could be due to inconsistent country naming scheme
+df_invalidValues = df[df.isnull().any(1)]
+df_invalidValues.to_csv('invalid_coordinates_dataframe.csv')
 
 # remove all rows with '*' as a value
 df = df[df.Value != '*']
