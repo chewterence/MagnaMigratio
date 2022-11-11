a807960abb5f07950f333bb4fc16b7081b3e2f3e
chewterence <chewterence@hotmail.com>
2022-11-11 15:00:27 +0800

    update readme

--- README.md
+++ README.md
@@ -1,75 +1,59 @@
 
 # Magna Migratio
+
 /ˈmaɲ.ɲa/;
+
 "Magna Migratio" or latin for "The Great Migration" is a data visualization project that aims to visualize UNHCR data.
 
 This project was created as part of a data visualization project for the GET1030/GEI1002 Computers and the Humanities course, in the National University of Singapore.
 
 The authors of this project are:
-- Terence Chew (chewterence@hotmail.com)
+
+- Terence Chew (chewterence@u.nus.edu)
+
 - Charlene
+
 - Daeho
+
 - Ho Yi Shu Keon
+
 - Pang Ji Xuan
 
-## Data Source
-UNHCR
+Any queries regarding the sources, mehtodology, implementation of the project should be directed to chewterence@u.nus.edu
 
-## Directory Structure
-
+## Navigating The Folders
+The following are the important subdirectories and a brief description of its contents:
+- **analysis**: contains the code that is used to perform network analysis
+	- line_coordinates.json is the raw file that the network analysis is performed
+	- A custom algorithm has been written to extract, process then analyse the data using pandas and seaborn
+- **data processing**: contains the code that is used to perform preprocessing of the dataset
+	- The data processing is abstracted into a three step approach.
+	- Run the python scripts in the order of data_preprocessor.py, secondary_data_processor.py, then webApp_json_processor.py
+	- resettlement_data.csv is the raw dataset obtained from UNHCR
+  
+## Data Source & References
+[UNHCR](https://www.kaggle.com/datasets/unitednations/refugee-data)
+[Dataset Publishing Language: countries.csv](https://developers.google.com/public-data/docs/canonical/countries_csv)
 
 ## Dependencies
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Data analysis done using [Python](https://www.python.org/) and [Pandas](https://pandas.pydata.org/).
+ [React Simple Maps](https://www.react-simple-maps.io/): `npm i -S react-simple-maps`
+ [Material UI](https://mui.com/): `npm install @mui/material @emotion/react @emotion/styled`
 
 ## Getting Started
-
-  
-
 In the main project directory, you can run:
 
-  
-
 ### `npm start`
-
-  
-
 Runs the app in the development mode.\
-
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-  
-
 The page will reload when you make changes.\
-
 You may also see any lint errors in the console.
 
-  
-
-### `npm test`
-
-  
-
-Launches the test runner in the interactive watch mode.\
-
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-  
-
 ### `npm run build`
-
-  
-
-Builds the app for production to the `build` folder.\
-
+Builds the app for production to the `build` folder.
 It correctly bundles React in production mode and optimizes the build for the best performance.
-
-  
-
 The build is minified and the filenames include the hashes.\
-
 Your app is ready to be deployed!
-
-  
-
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,34 +1,140 @@
 import json
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
   
 # Loading JSON file
 fp = open('line_coordinates.json')
 data = json.load(fp)
 
-# Select a year
-year = 1992
-data_year = data[str(year)]
+# Lists for dataframes
+years_list = []
+numEdges_list = []
+avg_degree_list = []
+total_list = []
+yearly_in_degree_map = {}
+yearly_out_degree_map = {}
 
-# Algorithm to extract basic details
-num_edges = len(data_year)
-in_degree_map = {}
-out_degree_map = {}
-nodes = set()
-for row in data_year:
-    vertex_origin = row[4]
-    vertex_destination = row[5]
-    nodes.add(vertex_origin)
-    nodes.add(vertex_destination)
-    if vertex_origin in out_degree_map:
-        out_degree_map[vertex_origin] += 1
-    else:
-        out_degree_map[vertex_origin] = 1
-    if vertex_destination in in_degree_map:
-        in_degree_map[vertex_destination] += 1
-    else:
-        in_degree_map[vertex_destination] = 1
-    
-num_nodes = len(nodes)
+# Discussion 1
+# Iterate through each year
+for year in data:
+    year_data = data[str(year)]
+    # Algorithm to extract basic details for each year
+    num_edges = len(year_data)
+    in_degree_map = {}
+    out_degree_map = {}
+    nodes = set()
+    total = 0
+    for row in year_data:
+        total += row[3]
+        vertex_origin = row[4]
+        vertex_destination = row[5]
+        nodes.add(vertex_origin)
+        nodes.add(vertex_destination)
+        if vertex_origin in out_degree_map:
+            out_degree_map[vertex_origin] += 1
+        else:
+            out_degree_map[vertex_origin] = 1
+        if vertex_destination in in_degree_map:
+            in_degree_map[vertex_destination] += 1
+        else:
+            in_degree_map[vertex_destination] = 1
+    num_nodes = len(nodes)
 
-print(num_nodes)
-print(num_edges)
-print(out_degree_map)
+    yearly_in_degree_map[year] = in_degree_map
+    yearly_out_degree_map[year] = out_degree_map
+    years_list.append(year)
+    numEdges_list.append(num_edges)
+    avg_degree_list.append(num_edges / len(nodes))
+    total_list.append(total)
+
+# Create dataframes
+d = {'Year': years_list,'Average Degree': avg_degree_list, 'total': total_list}
+df1 = pd.DataFrame(d)
+# Get year with the maximum and minimum average degree
+max_avgDegree = max(avg_degree_list)
+max_index = avg_degree_list.index(max_avgDegree)
+max_avgDegree_year = years_list[max_index]
+print("Year: " + max_avgDegree_year + " MAX avg_degree: " + str(max_avgDegree))
+min_avgDegree = min(avg_degree_list)
+min_index = avg_degree_list.index(min_avgDegree)
+min_avgDegree_year = years_list[min_index]
+print("Year: " + min_avgDegree_year + " MIN avg_degree: " + str(min_avgDegree))
+sns.lineplot(data=df1, x="Year", y="Average Degree").set(title='Line Plot of Average Degree Against Year From 1959-2016')
+plt.xticks(
+    df1['Year'].iloc[::2], # Odd rows only
+    rotation=45,
+    horizontalalignment='right',
+    fontweight='light'
+)
+plt.show()
+
+# Discussion 2
+# Extract details for in_degree distribution
+# Year 1969
+map = yearly_in_degree_map['1969']
+out_map = yearly_out_degree_map['1969']
+countries_list = []
+in_degree_list = []
+out_countries_list = []
+out_degree_list = []
+map = dict(sorted(map.items(), key=lambda item: item[1]))
+out_map = dict(sorted(out_map.items(), key=lambda item: item[1]))
+for country in map:
+    countries_list.append(country)
+    in_degree_list.append(map[country])
+for country in out_map:
+    out_countries_list.append(country)
+    out_degree_list.append(out_map[country])
+d = {'In Degree': in_degree_list, 'Country': countries_list}
+df_in_1969 = pd.DataFrame(d)
+d = {'Out Degree': out_degree_list, 'Country': out_countries_list}
+df_out_1969 = pd.DataFrame(d)
+# Year 2014
+map = yearly_in_degree_map['2014']
+out_map = yearly_out_degree_map['2014']
+countries_list = []
+in_degree_list = []
+out_countries_list = []
+out_degree_list = []
+map = dict(sorted(map.items(), key=lambda item: item[1]))
+out_map = dict(sorted(out_map.items(), key=lambda item: item[1]))
+for country in map:
+    countries_list.append(country)
+    in_degree_list.append(map[country])
+for country in out_map:
+    out_countries_list.append(country)
+    out_degree_list.append(out_map[country])
+d = {'In Degree': in_degree_list, 'Country': countries_list}
+df_in_2014 = pd.DataFrame(d)
+d = {'Out Degree': out_degree_list, 'Country': out_countries_list}
+df_out_2014 = pd.DataFrame(d)
+# Plot bar graphs
+sns.barplot(data=df_out_1969, x="Country", y="Out Degree").set(title='Out-degree Distribution Across Countries in 1969')
+plt.xticks(
+    rotation=45,
+    horizontalalignment='right',
+    fontweight='light'
+)
+plt.show()
+sns.barplot(data=df_out_2014, x="Country", y="Out Degree").set(title='Out-degree Distribution Across Countries in 2014')
+plt.xticks(
+    rotation=45,
+    horizontalalignment='right',
+    fontweight='light'
+)
+plt.show()
+sns.barplot(data=df_in_1969, x="Country", y="In Degree").set(title='In-degree Distribution Across Countries in 1969')
+plt.xticks(
+    rotation=45,
+    horizontalalignment='right',
+    fontweight='light'
+)
+plt.show()
+sns.barplot(data=df_in_2014, x="Country", y="In Degree").set(title='In-degree Distribution Across Countries in 2014')
+plt.xticks(
+    rotation=45,
+    horizontalalignment='right',
+    fontweight='light'
+)
+plt.show()
